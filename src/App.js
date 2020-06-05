6b3208d85ed42620f98d09589ec65a18fbe78d98
Ricard Villalba <ricardsax@gmail.com>
2020-06-05 10:52:55 +0200

    get done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    todos: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:4000/api/v1/todos").then((res) => {
+      console.log(res);
+      this.setState({
+        todos: res.data.slice(0, 10),
+      });
+    });
+  }
+
   render() {
+    const { todos } = this.state;
+    const todosList = todos.length ? (
+      todos.map((post) => {
+        return (
+          <div key={todos.id}>
+            <h1>{todos.title}</h1>
+            <h3>{todos.body}</h3>
+          </div>
+        );
+      })
+    ) : (
+      <div> empy </div>
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Todos list</h1>
+        {todosList}
       </div>
     );
   }
